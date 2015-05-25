18b768d63a499f97ac43ff5245fb7469360b04c9
Netanel Gilad <netanelgilad@gmail.com>
2015-05-26 00:09:31 +0300

    clean logic for image uploading only

--- README.md
+++ README.md
@@ -1,45 +1,4 @@
-# Angular/Meteor/CollectionFS issues
+Angular Meteor CollectionFS Example
+===========
 
-I created this project to expose some problems I currently have with the combination of Meteor, Angular, CollectionFS and Velocity.
-The main goal here is to contribute to make all these good things to work gracefully together. So here are the related bugs I intend to
-fix :
-
-## https://github.com/CollectionFS/DEPRECATING-cfs-file/issues/9
-
-The problem here is that when I updated the content of a store with a formatted content of another one, I have to change the updatedAt property
-manually.
-
-https://github.com/epotvin/thumbnail-example/blob/master/model.js#L24-L27
-
-The reason I want this to work is because I want the thumbnail to be updated reactively when someone update it from the original.
-
-## https://github.com/Urigo/angular-meteor/issues/72
-
-This issue is more a feature request then a bug. It would be really cool to deal with a cfs collection like a normal collection.
-After I have finish with this I intend to try this example with the change @netanelgilad has tried. It seems to be the fix I need.
-
-About these lines :
-
-https://github.com/epotvin/thumbnail-example/blob/master/client/example.js#L33-L40
-
-If I replace them with a simple line like this :
-
-```javascript
-return image.url({store: store}) + '&updatedAt=' + image.copies[store].updatedAt.getTime();
-````
-
-as I would have expected to be able to be, I fall in a digest loop throwing errors.
-
-About these : https://github.com/epotvin/thumbnail-example/blob/master/client/example.js#L45-L49
-
-I think the fix of @netanelgilad is exactly what will solve it.
-
-## https://github.com/Urigo/angular-meteor/issues/234
-
-When meteor starts, Karma have a hard time with angular and cfs. In the console of the second window opened by karma, I have the following errors :
-
-Failed to load resource: the server responded with a status of 404 (Not Found)
-cfs_access-point.js?0bfe974ee453abef55cf9a71244e13121aabc0a3:284 failed [404] NOT FOUND
-http://localhost:9876/client/example.ng.html Failed to load resource: the server responded with a status of 404 (Not Found)
-http://localhost:9876/client/image.ng.html Failed to load resource: the server responded with a status of 404 (Not Found)
-
+Small example that uses CollectionFS to upload images with angular-meteor.
--- client/example.js
+++ client/example.js
@@ -1,79 +1,34 @@
-angular.module('example', ['angular-meteor', 'ui.router']);
-
-angular.module("example").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
-    function ($urlRouterProvider, $stateProvider, $locationProvider) {
-
-        $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-
-        $stateProvider
-            .state('app', {
-                url: '/',
-                templateUrl: 'client/example.ng.html',
-                controller: 'ExampleCtrl'
-            })
-            .state('app.image', {
-                url: ':imageId',
-                templateUrl: 'client/image.ng.html',
-                controller: 'ImageCtrl'
-            });
-    }]);
+angular.module('example', ['angular-meteor']);
 
 angular.module('example').controller('ExampleCtrl', ['$meteor', '$scope', function ($meteor, $scope) {
 
-    $scope.images = $meteor.collection(function () {
-        return Images.find();
-    });
+    $scope.images = $meteor.collection(Images, false, Images);
 
     $scope.addImage = function () {
         $('<input type="file">').bind("change", function (event) {
-            Images.insert(event.target.files[0]);
+            // There is a bug in angular-meteor that causes this code not to
+            // work. Once it is solves should be possible with:
+            //$scope.images.save(event.target.files[0]);
+
+            // this is temporary
+            Images.insert(event.target.files[0], function(error, result) {
+                if (error) {
+                    console.log('error', error);
+                }
+                else {
+                    console.log(result);
+                }
+            });
         }).click();
+    };
+
+    $scope.removeImage = function(image) {
+      $scope.images.remove(image);
     };
 
     $scope.url = function (image, store) {
         if (!image) return null;
 
-        // Ideally, the following line would have worked
-        //return image.url({store: store});
-        var file = Images.findOne(image._id);
-        return file.url({store: store}) + '&updatedAt=' + file.copies[store].updatedAt.getTime();
+        return image.url({store: store});
     }
-
 }]);
-
-angular.module('example').controller('ImageCtrl', ['$scope', '$meteor', '$stateParams', function ($scope, $meteor, $stateParams) {
-    // Ideally, the following line would have worked
-    //$scope.image = $meteor.object(Images, $stateParams.imageId);
-
-    //Used this way, the image is not loader instantly on page refresh
-    $scope.image = Images.findOne($stateParams.imageId);
-
-    $scope.update = function() {
-        $meteor.call('setThumbnail', $scope.image._id, $scope.coordinates);
-    }
-
-}]);
-
-angular.module('example').directive('cropper', function () {
-    return {
-        restrict: 'A',
-        scope: {
-            coordinates: '='
-        },
-        link: function (scope, element) {
-            element.click(function () {
-                element.cropper({
-                    aspectRatio: 1,
-                    crop: function (data) {
-                        scope.$apply(function() {
-                            scope.coordinates = data;
-                        });
-                    }
-                });
-            });
-        }
-    }
-});
